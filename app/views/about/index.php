2bfbb127360c567e4ed65756ce8fd1f7408e4f9c
DwikiD <dwiki_173040069@mail.ac.id>
2019-10-10 00:42:24 +0700

    latihan php mvc asset

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
+<div class="container">
 
-	<h1>About Me</h1>
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/aku.jpg" alt="Dwiki Darmawan" width="200" class="rounded-circle shadow">
 	<p>Halo, nama saya adalah <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
